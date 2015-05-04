ae3c1cc7fa555ac9233b64269abedc72715be058
ohno <elena.khoroshash@gmail.com>
2015-05-05 01:51:20 +0400

    My Django Girls app

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,12 +109,13 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
